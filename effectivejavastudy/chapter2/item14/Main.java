8dca34f04d7aa886277a222d70bf1c7e63304de1
daily1313 <kimsb7218@naver.com>
2024-05-19 20:14:20 +0900

    feat: effectivejavastudy/chapter2/item14/Student.java [item-14]

---
+++ effectivejavastudy/chapter2/item14/Main.java
@@ -0,0 +1,26 @@
+package effectivejavastudy.chapter2.item14;
+
+import static effectivejavastudy.chapter2.item14.Student.COMPARATOR;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("c",1, 70));
+        students.add(new Student("b",2, 50));
+        students.add(new Student("a", 3, 20));
+
+        for(Student student: students) {
+            System.out.println(student);
+        }
+
+        students.sort(COMPARATOR);
+
+        for(Student student: students) {
+            System.out.println(student);
+        }
+    }
+}
---
+++ effectivejavastudy/chapter2/item14/Student.java
@@ -0,0 +1,35 @@
+package effectivejavastudy.chapter2.item14;
+
+import java.util.Comparator;
+
+public class Student implements Comparable<Student>{
+
+    public static final Comparator<Student> COMPARATOR =
+            Comparator.comparingDouble((Student student) -> student.score)
+                    .thenComparing((Student student) -> student.name)
+                    .thenComparingInt((Student student) -> student.id);
+
+    String name;
+    int id;
+    double score;
+
+    public Student(String name, int id, double score) {
+        this.name = name;
+        this.id = id;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", score=" + score +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return COMPARATOR.compare(this, o);
+    }
+}
